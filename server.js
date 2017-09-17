be07c7c87999eaf30aff819ac5fa0b16c0cd8e39
einarzb <einarzb@gmail.com>
2017-09-17 18:08:27 +0300

    added heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var User = require("./models/userModel")
 
 var app = express();
-mongoose.connect(process.env.CONNECTION_STRING||'mongodb://localhost/employees');
+mongoose.connect(process.env.CONNECTION_STRING ||'mongodb://localhost/employees');
 
 //middleware
 app.use(bodyParser.json());
