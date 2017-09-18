7f827f36b6327bd74c132594c78667e25d5ba1a1
einarzb <einarzb@gmail.com>
2017-09-18 22:24:52 +0300

    success in getting users address and passing it on

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -3,6 +3,11 @@ app.controller('mainController', function($scope, usersFactory) {
   $scope.editable = false;
   $scope.editMode = true;
 
+  $scope.searchAddress = function(address){
+    console.log(address);
+    usersFactory.getLatLng(address);
+    $scope.closeForm();
+  }
 
   var userCopy = this.copy;
 
@@ -34,55 +39,3 @@ app.controller('mainController', function($scope, usersFactory) {
     usersFactory.getUsers();
 
 });
-
-// app.controller('mainController', function($scope, usersFactory) {
-// //initializing
-//   $scope.editable = false;
-//   $scope.editMode = true;
-//   $scope.users = [];
-//
-//   var userCopy = this.copy;
-//
-//   //getting items from db
-//   $scope.getUsers = usersFactory.getUsers;
-//
-//   $scope.getUsers()
-//     .then(function(response){
-//       $scope.users = response;
-//     })
-//     .catch(function(error){
-//       console.log(error);
-//     })
-//
-//   $scope.addUser = function(newUser) {
-//     usersFactory.addToList(newUser)
-//     .then(function(response){
-//     $scope.users.push(response);
-//     })
-//     .catch(function(error){
-//       console.log(error);
-//     })
-//   };
-//
-//   $scope.removeFromList = usersFactory.removeFromList;
-//
-//   $scope.editItem = function(userCopy){
-//     this.editable = true;
-//     $scope.editMode = false;
-//
-//   };
-//
-//    $scope.closeForm = function(){
-//     $('#formModal').modal('hide');
-//   }
-//
-//   $scope.saveUser = function(id, updatedUser){
-//       usersFactory.saveUser(id, updatedUser);
-//       $scope.editMode = true;
-//       this.editable = false;
-//
-//   }
-//
-//     usersFactory.getUsers();
-//
-// });
