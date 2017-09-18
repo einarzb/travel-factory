d7bbcac332640fadfd9c9fc38e070ca40df6ff95
Arkadi <mrarkadi@gmail.com>
2017-09-19 02:42:19 +0300

    simplified controller

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -4,9 +4,10 @@ app.controller('mainController', function($scope, usersFactory) {
   $scope.editMode = true;
 
 
-  $scope.searchAddress = function(user){
+  $scope.createUserWithAddress = function(user) {
     usersFactory.getLatLng(user.address).then(function(response) {
         user.location = response;
+        $scope.addUser(user);
         $scope.closeForm();
       }, function(err) {
         console.error(err);
@@ -41,11 +42,11 @@ app.controller('mainController', function($scope, usersFactory) {
 
   }
 
-  $scope.createUserWithAddress = function(newUser) {
-    $scope.searchAddress(newUser);
-    setTimeout(function(){ console.log(newUser); $scope.addUser(newUser); }, 1000);
-    // setTimeout(function(){ console.log(usr.$$state.value.address); $scope.searchAddress(usr.$$state.value.address) }, 1000);
-  }
+  // $scope.createUserWithAddress = function(newUser) {
+  //   $scope.searchAddress(newUser);
+  //   setTimeout(function(){ console.log(newUser); $scope.addUser(newUser); }, 1000);
+  //   // setTimeout(function(){ console.log(usr.$$state.value.address); $scope.searchAddress(usr.$$state.value.address) }, 1000);
+  // }
 
   usersFactory.getUsers();
 
